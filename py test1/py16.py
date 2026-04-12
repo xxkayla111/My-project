e3ba97aa270cb7e6a1a2ecd7b91fff641d37baf4
Cheng Cong <2713555264@qq.com>
2026-04-12 16:09:48 +0800

    4.12

--- py test1/py16.py
+++ py test1/py16.py
@@ -25,12 +25,43 @@
 # 2.通过装饰器实现
 # 3.通过重写__new__()实现  (重点)
 # 4.通过导入模块实现
-class A(object):
-    pass
-a1 = A()
-print(a1)
-a2 = A()
-print(a2)
+# class A(object):
+#     pass
+# a1 = A()
+# print(a1)
+# a2 = A()
+# print(a2)
 #内存地址发生变化,说明是不同的对象
 #实现单例模式,对象的地址都是一样的,只有一个对象
 # 2.3通过重写__new__方法()实现单例模式
+#设计流程
+# 1.定义一个类属性,初始值为None,用来记录单例对象的引用
+# 2.重写__new__()方法
+# 3.进行判断,如果类属性是None,把__new__()返回的对象引用保存进去
+# 4.返回类属性中记录的对象引用
+# class Singleton(object):
+#     #记录第一个被创建的对象的引用
+#     obj = None
+
+#     def __new__(cls,*args,**kwargs):
+#         print("这是__new__()方法")
+#         #判断类属性是否为空
+#         if cls.obj == None:
+#             cls.obj = super().__new__(cls) 
+        
+#         return cls.obj
+
+#     def __init__(self):
+#         print("这是__init__()方法")
+
+# s = Singleton()
+# print(s)
+# s2 = Singleton()
+# print(s2)   
+# 单例模式：每一次实例化所创建的对象都是同一个，内存地址都一样
+
+# 2.4  通过模块导入实现单例模式
+from pytest02 import te as te01
+print(te01,id(te01))
+from pytest02 import te as te02
+print(te02,id(te02))
---
+++ py test1/pytest02.py
@@ -0,0 +1,5 @@
+class Test(object):
+    def test(self):
+        print("这是test")
+        
+te = Test()
